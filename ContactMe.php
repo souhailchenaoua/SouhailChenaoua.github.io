0b708fb3775ced8f0edfd79ded23e9cd181ef0f6
Souheil Idris Chenaoua <78444908+souhailchenaoua@users.noreply.github.com>
2021-07-07 16:35:35 +0300

    Add files via upload

--- ContactMe.php
+++ ContactMe.php
@@ -2,13 +2,12 @@
 if(isset($_POST['submit'])){
 $name =$_POST['name'];
 $mailfrom =$_POST['mail'];
-$message= $_POST['message'];
-
+$message= $_POST['message']; 
 $mailTo = "souhail.chenaoua@gmail.com";
 $headers = "From: ".$mailfrom;
 $txt = "you have recieved an e-mail from" .$name ."\n\n". $message;
 mail($mailTo, $txt, $headers);
-header("location: index.php?mailsend");
+header("location: index.html");
 
 }
 ?>
